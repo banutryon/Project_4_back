d42054bd74375f4554d3df918424dd334ecbaf01
banu tryon <banutryon@banus-mbp.lan>
2021-01-28 15:52:49 -0800

    seed test

---
+++ locations/db/testseed.rb
@@ -0,0 +1,9 @@
+CREATE TABLE locations (id SERIAL, name VARCHAR(50), street VARCHAR(255), city VARCHAR(160), state VARCHAR(2), zip VARCHAR(5) img VARCHAR(300));
+#seed info Example:
+INSERT INTO locations (name, street, city, state, img) VALUES ('Tree House', '5802 134th Pl SE', 'Everett', 'WA','98208' 'https://media.glampinghub.com/CACHE/images/accommodations/private-tree-house-retreat-overlooking-the-columbia-river-in-washi-1562673467304/cf09d6e633516ddd3274412e337f34dc.jpg');
+
+
+
+INSERT INTO locations (name, street, city, state, img) VALUES ('', '', '', '', '', '');
+INSERT INTO locations (name, street, city, state, img) VALUES ('', '', '', '', '', '');
+INSERT INTO locations (name, street, city, state, img) VALUES ('', '', '', '', '', '');
