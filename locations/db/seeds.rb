0fb2f7a630b3c95f74206b5519db0703c98fcabe
banu tryon <banutryon@banus-mbp.lan>
2021-01-29 08:28:04 -0800

    trying to load seed data

--- locations/db/seeds.rb
+++ locations/db/seeds.rb
@@ -5,9 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-CREATE TABLE locations (id SERIAL, name VARCHAR(20), street VARCHAR(20), city VARCHAR(20), state VARCHAR(2), zipcode INT, img1 VARCHAR(200), img2 VARCHAR(200), img3 VARCHAR(200), description VARCHAR(500),);
-INSERT INTO locations (name, street, city, state, zipcode, img1, img2, img3, description) VALUES ("Tree house", "6922 Preston-Fall City Rd SE", "Issaquah", "WA", 98027 , "https://www.treehousepoint.com/images/Upper-Pond-2018-banner.jpg", "https://www.treehousepoint.com/images/upper-pond/Upper-TreeHouse-Point-10.jpg", "https://www.treehousepoint.com/images/upper-pond/Upper-TreeHouse-Point-sh-6.jpg", "This treehouse sleeps 3 with ease. At 200sqft your tiny home awaits you.");
-INSERT INTO locations (name, street, city, state, zipcode, img1, img2, img3, description) VALUES ("Safari Tent", "NA", "Whidbey Island", "WA", 98277 , "https://media.glampinghub.com/CACHE/images/accommodations/romantic-glamping-tent-rentals-on-whidbey-island-in-greenbank-was-1557747636489/8573666dc063cea4ec69848f22a68398.jpg" , "https://media.glampinghub.com/CACHE/images/accommodations/romantic-glamping-tent-rentals-on-whidbey-island-in-greenbank-was-1559580105738/d4448b2411c5a19cf21dc4c287a113b0.jpg", "https://media.glampinghub.com/CACHE/images/accommodations/romantic-glamping-tent-rentals-on-whidbey-island-in-greenbank-was-1559643869110/c2759d253e4e1578740e06402c680f38.jpg", "There are two romantic glamping tent rentals available at this property centrally located on Whidbey Island in Greenbank, Washington.");
-SELECT * FROM locations,
 
+
+# CREATE TABLE locations (id SERIAL, name VARCHAR(20), street VARCHAR(20), city VARCHAR(20), state VARCHAR(2), zipcode INT, img1 VARCHAR(200), img2 VARCHAR(200), img3 VARCHAR(200), description VARCHAR(500),);
+# INSERT INTO locations (name, street, city, state, zipcode, img1, img2, img3, description) VALUES ("Tree house", "6922 Preston-Fall City Rd SE", "Issaquah", "WA", 98027 , "https://www.treehousepoint.com/images/Upper-Pond-2018-banner.jpg", "https://www.treehousepoint.com/images/upper-pond/Upper-TreeHouse-Point-10.jpg", "https://www.treehousepoint.com/images/upper-pond/Upper-TreeHouse-Point-sh-6.jpg", "This treehouse sleeps 3 with ease. At 200sqft your tiny home awaits you.");
+# INSERT INTO locations (name, street, city, state, zipcode, img1, img2, img3, description) VALUES ("Safari Tent", "NA", "Whidbey Island", "WA", 98277 , "https://media.glampinghub.com/CACHE/images/accommodations/romantic-glamping-tent-rentals-on-whidbey-island-in-greenbank-was-1557747636489/8573666dc063cea4ec69848f22a68398.jpg" , "https://media.glampinghub.com/CACHE/images/accommodations/romantic-glamping-tent-rentals-on-whidbey-island-in-greenbank-was-1559580105738/d4448b2411c5a19cf21dc4c287a113b0.jpg", "https://media.glampinghub.com/CACHE/images/accommodations/romantic-glamping-tent-rentals-on-whidbey-island-in-greenbank-was-1559643869110/c2759d253e4e1578740e06402c680f38.jpg", "There are two romantic glamping tent rentals available at this property centrally located on Whidbey Island in Greenbank, Washington.");
+# SELECT * FROM locations,
+
+
+
+Location.create([
+
+    {name: "Tree house", street: "6922 Preston-Fall City Rd SE", city: "Issaquah", state: "WA", zipcode: 98027 , img1: "https://www.treehousepoint.com/images/Upper-Pond-2018-banner.jpg", img2: "https://www.treehousepoint.com/images/upper-pond/Upper-TreeHouse-Point-10.jpg", img3: "https://www.treehousepoint.com/images/upper-pond/Upper-TreeHouse-Point-sh-6.jpg", description: "This treehouse sleeps 3 with ease. At 200sqft your tiny home awaits you." },
+
+    {name: "Safari Tent", street: "NA", city: "Whidbey Island", state: "WA", zipcode: 98277 , img1: "https://media.glampinghub.com/CACHE/images/accommodations/romantic-glamping-tent-rentals-on-whidbey-island-in-greenbank-was-1557747636489/8573666dc063cea4ec69848f22a68398.jpg" , img2: "https://media.glampinghub.com/CACHE/images/accommodations/romantic-glamping-tent-rentals-on-whidbey-island-in-greenbank-was-1559580105738/d4448b2411c5a19cf21dc4c287a113b0.jpg", img3: "https://media.glampinghub.com/CACHE/images/accommodations/romantic-glamping-tent-rentals-on-whidbey-island-in-greenbank-was-1559643869110/c2759d253e4e1578740e06402c680f38.jpg", description: "There are two romantic glamping tent rentals available at this property centrally located on Whidbey Island in Greenbank, Washington." },
+    ])
 
