476c80b7ee84b4cfaea10a2831ff63ae334fecc3
Ryan Fanelli <illenafnayr@gmail.com>
2021-01-30 12:53:03 -0500

    asdf

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,9 +1,10 @@
 class Location < ApplicationRecord
 
-    if ENV["DATABASE_URL"]
-        PG.connect(ENV['DATABASE_URL'])
-      elsif
-        DB = PG.connect({ :port => 5432, :dbname => 'locations_development'})
+    if(ENV['DATABASE_URL'])
+        uri = URI.parse(ENV['DATABASE_URL'])
+        DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
+      else
+        DB = PG.connect(host: "localhost", port: 5432, dbname: 'locations_development')
       end
     # DB = PG.connect({ :port => 5432, :dbname => 'locations_development' #asdf})
 
