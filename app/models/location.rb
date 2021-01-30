87a9ccffdd13a865dd17d9e37a004118c21f60b2
Ryan Fanelli <illenafnayr@gmail.com>
2021-01-30 13:36:55 -0500

    heroku

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,6 +4,7 @@ class Location < ApplicationRecord
         uri = URI.parse(ENV['DATABASE_URL'])
         DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
       else
+        DB = PG.connect( port: 5432, dbname: 'locations_development')
       end
 
       def self.find(id)
@@ -101,9 +102,6 @@ class Location < ApplicationRecord
               :description => results.first["description"] 
           }
       end
-  endDB = PG.connect(host: "localhost", port: 5432, dbname: 'locations_development')
-      end
-    # DB = PG.connect({ :port => 5432, :dbname => 'locations_development' #asdf})
 
     def self.all
         results = DB.exec("SELECT * FROM locations;")
