f9a829540d148443769e761d3af488233d254b1b
Ryan Fanelli <illenafnayr@gmail.com>
2021-01-30 16:11:08 -0500

    connect db heroku

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -1,5 +1,4 @@
 class Seller < ApplicationRecord
-
     if(ENV['DATABASE_URL'])
         uri = URI.parse(ENV['DATABASE_URL'])
         DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
