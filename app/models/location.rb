47a8fefccf20c0c3a36ffa36ab2c4a8101da7772
Ryan Fanelli <illenafnayr@gmail.com>
2021-01-30 20:38:08 -0500

    removed duplicate methods on locations model, and updated to incorporate lat and lng in locations model

--- app/models/location.rb
+++ app/models/location.rb
@@ -5,22 +5,7 @@ class Location < ApplicationRecord
       else
         DB = PG.connect(host: "localhost", port: 5432, dbname: 'locations_development')
       end
-    # DB = PG.connect({:host => "localhost", :port => 5432, :dbname => 'locations_development'})
-      def self.find(id)
-          results = DB.exec("SELECT * FROM locations WHERE id=#{id};")
-          return {
-              :id => results.first["id"],
-              :name => results.first["name"],
-              :street => results.first["street"],
-              :city => results.first["city"],
-              :state => results.first["state"],
-              :zipcode => results.first["zipcode"],
-              :img1 => results.first["img1"],
-              :img2 => results.first["img2"],
-              :img3 => results.first["img3"],
-              :description => results.first["description"]
-          }
-      end    
+    # DB = PG.connect({:host => "localhost", :port => 5432, :dbname => 'locations_development'})    
       
       def self.create(new)
           results = DB.exec(
@@ -31,6 +16,8 @@ class Location < ApplicationRecord
                       city,
                       state,
                       zipcode,
+                      lat,
+                      lng,
                       img1,
                       img2,
                       img3,
@@ -42,12 +29,14 @@ class Location < ApplicationRecord
                       '#{new["city"]}',
                       '#{new["state"]}',
                       #{new["zipcode"]},
+                      #{new["lat"]},
+                      #{new["lng"]},
                       '#{new["img1"]}',
                       '#{new["img2"]}',
                       '#{new["img3"]}',
                       '#{new["description"]}'
                   )
-                  RETURNING id, name, street, city, state, zipcode, img1, img2, img3, description;
+                  RETURNING id, name, street, city, state, zipcode, lat, lng, img1, img2, img3, description;
               SQL
           )
           return {
@@ -57,6 +46,8 @@ class Location < ApplicationRecord
               :city => results.first["city"],
               :state => results.first["state"],
               :zipcode => results.first["zipcode"],
+              :lat => results.first["lat"],
+              :lng => results.first["lng"],
               :img1 => results.first["img1"],
               :img2 => results.first["img2"],
               :img3 => results.first["img3"],
@@ -80,12 +71,14 @@ class Location < ApplicationRecord
                       city='#{new["city"]}',
                       state='#{new["state"]}',
                       zipcode='#{new["zipcode"]}',
+                      lat='#{new["lat"]}',
+                      lng='#{new["lng"]}',
                       img1='#{new["img1"]}',
                       img2='#{new["img2"]}',
                       img3='#{new["img3"]}',
                       description='#{new["description"]}'
                   WHERE id=#{id}
-                  RETURNING id, name, street, city, state, zipcode, img1, img2, img3, description;    
+                  RETURNING id, name, street, city, state, zipcode, lat, lng, img1, img2, img3, description;    
               SQL
           )
           return {
@@ -95,6 +88,8 @@ class Location < ApplicationRecord
               :city => results.first["city"],
               :state => results.first["state"],
               :zipcode => results.first["zipcode"].to_i,
+              :lat => results.first["lat"],
+              :lng => results.first["lng"],
               :img1 => results.first["img1"],
               :img2 => results.first["img2"],
               :img3 => results.first["img3"],
@@ -120,6 +115,7 @@ class Location < ApplicationRecord
         end
     end
 
+
     def self.find(id)
         results = DB.exec("SELECT * FROM locations WHERE id=#{id};")
         return {
@@ -129,90 +125,12 @@ class Location < ApplicationRecord
             :city => results.first["city"],
             :state => results.first["state"],
             :zipcode => results.first["zipcode"].to_i,
+            :lat => results.first["lat"],
+            :lng => results.first["lng"],
             :img1 => results.first["img1"],
             :img2 => results.first["img2"],
             :img3 => results.first["img3"],
             :description => results.first["description"]
-        }
-    end    
-    
-    def self.create(new)
-        results = DB.exec(
-            <<-SQL
-                INSERT INTO locations (
-                    name,
-                    street,
-                    city,
-                    state,
-                    zipcode,
-                    img1,
-                    img2,
-                    img3,
-                    description
-                )
-                VALUES (
-                    '#{new["name"]}',
-                    '#{new["street"]}',
-                    '#{new["city"]}',
-                    '#{new["state"]}',
-                    #{new["zipcode"]},
-                    '#{new["img1"]}',
-                    '#{new["img2"]}',
-                    '#{new["img3"]}',
-                    '#{new["description"]}'
-                )
-                RETURNING id, name, street, city, state, zipcode, img1, img2, img3, description;
-            SQL
-        )
-        return {
-            :id => results.first["id"].to_i,
-            :name => results.first["name"],
-            :street => results.first["street"],
-            :city => results.first["city"],
-            :state => results.first["state"],
-            :zipcode => results.first["zipcode"].to_i,
-            :img1 => results.first["img1"],
-            :img2 => results.first["img2"],
-            :img3 => results.first["img3"],
-            :description => results.first["description"] 
-        }
-    
-    end
-
-    def self.delete(id)
-        results = DB.exec("DELETE FROM locations WHERE id=#{id}")
-        return { "deleted" => true}
-    end
-
-    def self.update(id, new)
-        results = DB.exec(
-            <<-SQL
-                UPDATE locations
-                SET
-                    name='#{new["name"]}',
-                    street='#{new["street"]}',
-                    city='#{new["city"]}',
-                    state='#{new["state"]}',
-                    zipcode='#{new["zipcode"]}',
-                    img1='#{new["img1"]}',
-                    img2='#{new["img2"]}',
-                    img3='#{new["img3"]}',
-                    description='#{new["description"]}'
-                WHERE id=#{id}
-                RETURNING id, name, street, city, state, zipcode, img1, img2, img3, description;    
-            SQL
-        )
-        return {
-            :id => results.first["id"].to_i,
-            :name => results.first["name"],
-            :street => results.first["street"],
-            :city => results.first["city"],
-            :state => results.first["state"],
-            :zipcode => results.first["zipcode"].to_i,
-            :img1 => results.first["img1"],
-            :img2 => results.first["img2"],
-            :img3 => results.first["img3"],
-            :description => results.first["description"] 
         }
     end
 end
